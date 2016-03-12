96a0a90df9230e32a54d61b28b849f909b73a391
mhsun1992 <mhsun1992@gmail.com>
2016-03-12 10:36:17 +0800

    lsdjfl

--- projects/todali/views/common.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    $(window).scroll(function() {
-        if ($(window).scrollTop()) {
-            $("footer").slideDown();
-        } else {
-            $("footer").slideUp();
-        }
-    });
-})();
